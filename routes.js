a873af6776c25121f8925433933b7dfc2646095d
Pavel Zachesa <zachesa@ya.ru>
2019-02-10 21:11:01 +1000

    Исправил роутинг

---
+++ routes.js
@@ -0,0 +1,32 @@
+import React, { Component, Fragment } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import { Router } from 'react-router'
+
+import history from './history'
+
+import App from './containers/App'
+import Channel from './components/channel/Channel'
+import SignIn from './components/session/SignIn'
+
+import Auth from './auth'
+
+class Routes extends Component {
+
+  render() {
+
+    return (
+    <Router history={history} >
+      <Fragment id='content'>
+        <Route exact path="/" component={App} onChange={::Auth.requireAuth}/>
+        <Switch path="/channels">
+          <Route path="@:id" component={Channel} />
+          <Route path=":id" component={Channel} />
+        </Switch>
+        <Route path="/login" component={SignIn} onChange={::Auth.rejectAuth}/>
+      </Fragment>
+    </Router>
+    )
+  }
+}
+
+export default (Routes)
